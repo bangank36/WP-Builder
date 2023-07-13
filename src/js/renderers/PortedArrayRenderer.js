53de6572033beb62c25e52bbe2df1bd5eb0a6f87
bangank36 <bangank36@gmail.com>
2023-07-13 11:25:21 +0700

    Restore PortedArrayRenderer as reference

--- src/js/renderers/PortedArrayRenderer.js
+++ src/js/renderers/PortedArrayRenderer.js
@@ -1,19 +1,15 @@
 /*
   The MIT License
-
   Copyright (c) 2017-2019 EclipseSource Munich
   https://github.com/eclipsesource/jsonforms
-
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
   in the Software without restriction, including without limitation the rights
   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
   copies of the Software, and to permit persons to whom the Software is
   furnished to do so, subject to the following conditions:
-
   The above copyright notice and this permission notice shall be included in
   all copies or substantial portions of the Software.
-
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -219,52 +215,28 @@ export const ArrayControlRenderer = ({
 
   const [screenContent, setScreenContent] = useContext(NavigatorContext);
 
-    useEffect(() => {
-        // Use the callback since the new state is based on the previous state
-        setScreenContent(prevScreenContent => ({
-            ...prevScreenContent,
-            [`${route}`]: {
-                component: (<ArrayControl
-                    classNames={classNames}
-                    data={data}
-                    label={label}
-                    path={path}
-                    schema={schema}
-                    errors={errors}
-                    addItem={addItem}
-                    removeItems={removeItems}
-                    moveUp={moveUp}
-                    moveDown={moveDown}
-                    uischema={uischema}
-                    uischemas={uischemas}
-                    getStyleAsClassName={getStyleAsClassName}
-                    rootSchema={rootSchema}
-                    id={id}
-                    visible={visible}
-                    enabled={enabled}
-                    getStyle={getStyle}
-                    translations={translations}
-                  />),
-                label: uischema.label,
-                path: path
-            },
-        }))
-    });
-
   return (
-    <NavigationButtonAsItem
-        path={`${route}`}
-        aria-label={uischema.label}
-    >
-        <HStack justify="space-between">
-        <FlexItem>
-            Edit {uischema.label}
-        </FlexItem>
-        <IconWithCurrentColor
-            icon={isRTL() ? chevronLeft : chevronRight}
-        />
-        </HStack>
-    </NavigationButtonAsItem>
+    <ArrayControl
+      classNames={classNames}
+      data={data}
+      label={label}
+      path={path}
+      schema={schema}
+      errors={errors}
+      addItem={addItem}
+      removeItems={removeItems}
+      moveUp={moveUp}
+      moveDown={moveDown}
+      uischema={uischema}
+      uischemas={uischemas}
+      getStyleAsClassName={getStyleAsClassName}
+      rootSchema={rootSchema}
+      id={id}
+      visible={visible}
+      enabled={enabled}
+      getStyle={getStyle}
+      translations={translations}
+    />
   )
 }
 
