2366598e0198fa46d81abbf821741090230e8957
bangank36 <bangank36@gmail.com>
2023-07-25 09:47:48 +0700

    - Amend style and label position for * Checkbox * Toggle * Color * Text controls

--- src/js/renderers/Primitive/BooleanCheckboxControl.js
+++ src/js/renderers/Primitive/BooleanCheckboxControl.js
@@ -1,39 +1,58 @@
 import React from "react";
 import { withJsonFormsControlProps } from "@jsonforms/react";
 import { rankWith, isBooleanControl, optionIs, and } from "@jsonforms/core";
-import { CheckboxControl as UiCheckboxControl } from '@wordpress/components';
+import {
+	__experimentalHStack as HStack,
+	CheckboxControl as UiCheckboxControl,
+	Tooltip,	
+    FlexItem,
+	__experimentalSpacer as Spacer,
+} from '@wordpress/components';
 
 const CheckboxControl = (props) => {
   	const {
 		id,
 		description,
-		errors,
 		label,
-		uischema,
 		path,
-		visible,
-		required,
-		config,
 		data,
-		input,
 		handleChange
   	} = props;
   
   	return ( 
-		<UiCheckboxControl
-			checked={ !!data }
-			help={description}
-			label={label}
-			onChange={(value) =>
-				handleChange(path, value === '' ? undefined : value)
-			}
-		/>
+		<>
+			<Spacer paddingY={ 2 } marginBottom={ 0 }>
+				<HStack justify="space-between">
+					<FlexItem>
+					{ description ? (
+						<Tooltip text={ description }>
+						<label htmlFor={ id }>
+							{ label }
+						</label>
+					</Tooltip>
+					) : ( 
+						<label htmlFor={ id }>
+							{ label }
+						</label> 
+					) }
+					</FlexItem>
+					<FlexItem>
+						<UiCheckboxControl
+							checked={ !!data }
+							onChange={( value ) =>
+								handleChange( path, value === '' ? undefined : value )
+							}
+						/>
+					</FlexItem>
+				</HStack>
+			</Spacer>
+		</>
   	)
 };
 
 export const booleanCheckboxControlTester = rankWith(
 	5, //increase rank as needed
-	and(isBooleanControl, optionIs('toggle', false))
+	and( isBooleanControl, optionIs( 'toggle', false ) )
 );
 
-export default withJsonFormsControlProps(CheckboxControl);
+export default withJsonFormsControlProps( CheckboxControl );
