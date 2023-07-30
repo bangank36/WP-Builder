23813a141a88e79448c696edfc14ebf94f65b578
bangank36 <bangank36@gmail.com>
2023-07-30 11:59:22 +0700

    Update control tester for alignment matrix

--- src/js/renderers/Primitive/AlignmentMatrixControl.js
+++ src/js/renderers/Primitive/AlignmentMatrixControl.js
@@ -1,20 +1,22 @@
 import React from "react";
 import { withJsonFormsControlProps } from "@jsonforms/react";
-import { rankWith, isStringControl } from "@jsonforms/core";
 import { 
-	__experimentalAlignmentMatrixControl as AlignmentMatrixControl,
-    __experimentalVStack as VStack,
-	__experimentalHStack as HStack,
-	FormToggle,
+	rankWith, 
+	isStringControl,
+	and,
+	optionIs 
+} from "@jsonforms/core";
+import { 
+	Button,
+	Popover,
 	Tooltip,	
     FlexItem,
-	__experimentalSpacer as Spacer,
-
-	__experimentalDropdownContentWrapper as DropdownContentWrapper,
 	Dropdown,
-	Button,
 	SlotFillProvider, 
-	Popover,
+	__experimentalHStack as HStack,
+	__experimentalSpacer as Spacer,
+	__experimentalDropdownContentWrapper as DropdownContentWrapper,
+	__experimentalAlignmentMatrixControl as AlignmentMatrixControl,
 } from '@wordpress/components';
 
 const AlignmentMatrixControlRenderer = ( props ) => {
@@ -27,65 +29,68 @@ const AlignmentMatrixControlRenderer = ( props ) => {
 		handleChange
 	} = props;
   
-  return ( 
-    <>
-		<Spacer paddingY={ 2 } marginBottom={ 0 }>
-			<HStack justify="space-between">
-				<FlexItem>
-				{ description ? (
-					<Tooltip text={ description }>
-					<label htmlFor={ id }>
-						{ label }
-					</label>
-				</Tooltip>
-				) : ( 
-					<label htmlFor={ id }>
-						{ label }
-					</label> 
-				) }
-				</FlexItem>
-				<FlexItem>
-					<Dropdown
-						popoverProps={ {
-							placement: 'left-start',
-							offset: 36,
-							shift: true,
-						} }
-						className="my-container-class-name"
-						contentClassName="my-dropdown-content-classname"
-						renderToggle={ ( { isOpen, onToggle } ) => (
-							<Button
-								onClick={ onToggle }
-								aria-expanded={ isOpen }
-								icon={ 
-									<AlignmentMatrixControl.Icon
-										value = { data }
-									/>  
-								}
-							/>
-						) }
-						renderContent={ () => (
-							<DropdownContentWrapper paddingSize="none">
-								<SlotFillProvider>
-									<AlignmentMatrixControl
-										value = { data }
-										onChange = { ( value ) => handleChange( path, value ) }
-									/>
-									<Popover.Slot />
-								</SlotFillProvider>
-							</DropdownContentWrapper>
-						) }
-					/>
-				</FlexItem>
-			</HStack>
-		</Spacer>
-	</>
-  )
+  	return ( 
+		<>
+			<Spacer paddingY={ 2 } marginBottom={ 0 }>
+				<HStack justify="space-between">
+					<FlexItem>
+					{ description ? (
+						<Tooltip text={ description }>
+						<label htmlFor={ id }>
+							{ label }
+						</label>
+					</Tooltip>
+					) : ( 
+						<label htmlFor={ id }>
+							{ label }
+						</label> 
+					) }
+					</FlexItem>
+					<FlexItem>
+						<Dropdown
+							popoverProps={ {
+								placement: 'left-start',
+								offset: 36,
+								shift: true,
+							} }
+							className="my-container-class-name"
+							contentClassName="my-dropdown-content-classname"
+							renderToggle={ ( { isOpen, onToggle } ) => (
+								<Button
+									onClick={ onToggle }
+									aria-expanded={ isOpen }
+									icon={ 
+										<AlignmentMatrixControl.Icon
+											value = { data }
+										/>  
+									}
+								/>
+							) }
+							renderContent={ () => (
+								<DropdownContentWrapper paddingSize="none">
+									<SlotFillProvider>
+										<AlignmentMatrixControl
+											value = { data }
+											onChange = { ( value ) => handleChange( path, value ) }
+										/>
+										<Popover.Slot />
+									</SlotFillProvider>
+								</DropdownContentWrapper>
+							) }
+						/>
+					</FlexItem>
+				</HStack>
+			</Spacer>
+		</>
+  	)
 };
 
 export const alignmentMatrixControlTester = rankWith(
 	5, //increase rank as needed
-	isStringControl
+	and(
+		isStringControl,
+		optionIs('format', 'alignment-matrix')
+	)
 );
 
 export default withJsonFormsControlProps( AlignmentMatrixControlRenderer );
