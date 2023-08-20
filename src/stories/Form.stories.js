d3780553fc1e1f08e6ae724710f1af37c4e528bf
bangank36 <bangank36@gmail.com>
2023-08-20 09:48:20 +0700

    Import examples package and clean up stories

--- src/stories/Form.stories.js
+++ src/stories/Form.stories.js
@@ -1,12 +1,18 @@
-import { Button } from './Button';
+import React, { useState } from "react";
+import { issue_1948 } from '@jsonforms/examples' 
+import {
+    materialRenderers,
+    materialCells,
+} from "@jsonforms/material-renderers";
+import { JsonForms } from "@jsonforms/react";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Example/Form',
-  component: Button,
+  component: JsonForms,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'centered',
+    layout: 'padded',
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
@@ -16,10 +22,84 @@ export default {
   }
 };
 
+const schema = {
+    type: "object",
+    properties: {
+      address: {
+        type: 'object',
+        properties: {
+          street_address: { 
+            type: 'string',
+          },
+          city: { type: 'string' },
+          state: { type: 'string' },
+          isOffice: { 
+            type: 'boolean',
+            description: 'Is this an office address?',
+          },
+          registeredDate: {
+            type: 'string',
+            format: 'date',
+          },
+          roofColor: {
+            type: 'string',
+            format: 'color',
+          },
+          country: {
+            type: 'object',
+            properties: {
+              name: { type: 'string' },
+            }
+          },
+          gender: {
+            type: "string",
+            enum: [ "male", "female", "other" ],
+            description: "The gender of the user"
+          },
+          race: {
+            type: 'string',
+            oneOf: [
+              { const: 'asian', title: 'Asian' },
+              { const: 'latin', title: 'Latin' },
+            ],
+          },
+          businessHours: {
+            type: 'array',
+            items: {
+              type: 'object',
+              properties: {
+                date: { 
+                  type: 'string',
+                },
+              }
+              
+            },
+          },
+        }
+      },
+      business: {
+        type: 'object',
+        properties: {
+          job: {
+            type: 'string',
+          },
+          experience: {
+            type: 'string',
+            maxLength: 5,
+          },
+        },
+      }
+    },
+};
+
+// Export const in for loop
+
+
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary = {
   args: {
-    primary: true,
-    label: 'Button',
+    schema: issue_1948.schema,
+    renderers: [...materialRenderers],
+    cells: [...materialCells]
   },
 };
