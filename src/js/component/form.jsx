14bc9a15a0cf0bda4dd23f29c0bf5b5ebc8dced0
bangank36 <bangank36@gmail.com>
2024-05-29 16:58:54 +0700

    - Add different schema

--- src/js/component/form.jsx
+++ src/js/component/form.jsx
@@ -71,8 +71,258 @@ const schema = {
         experience: {
           type: 'string',
           maxLength: 5,
-        },
+        }
       },
+    },
+    showOnHover: {
+      type: 'boolean',
+      description: 'Show Drawer panel on cart icon or link hover'
+    },
+    position: {
+      type: 'string',
+      enum: ['right', 'left'],
+      description: 'Position of the drawer'
+    },
+    width: {
+      type: 'string',
+      description: 'CSS valid drawer width'
+    },
+    height: {
+      type: 'string',
+      description: 'CSS valid drawer height'
+    },
+    margin: {
+      type: 'string',
+      description: 'CSS valid margin for drawer container'
+    },
+    effect: {
+      type: 'string',
+      enum: ['slide', 'fade'],
+      description: 'Appearing effect of the drawer'
+    },
+    effectDuration: {
+      type: 'string',
+      description: 'Appearing effect duration in ms or s'
+    },
+    effectEasing: {
+      type: 'string',
+      description: 'Valid CSS easing, like: ease, linear, cubic-bezier'
+    },
+    backgroundColor: {
+      type: 'string',
+      format: 'color',
+      description: 'Drawer background color'
+    },
+    color: {
+      type: 'string',
+      format: 'color',
+      description: 'Drawer main text color'
+    },
+    basicFontSize: {
+      type: 'string',
+      description: 'Drawer base font size'
+    },
+    basicFontFamily: {
+      type: 'string',
+      description: 'Set the drawer font'
+    },
+    useCustomScrollbar: {
+      type: 'boolean',
+      description: 'Use custom scrollbar, works in webkit browsers only'
+    },
+    scrollbarTrackColor: {
+      type: 'string',
+      format: 'color',
+      description: 'Webkit browsers scrolltrack color'
+    },
+    scrollbarThumbColor: {
+      type: 'string',
+      format: 'color',
+      description: 'Webkit browsers scrollthumb color'
+    },
+    scrollbarWidth: {
+      type: 'string',
+      description: 'Webkit browsers scrollbar width'
+    },
+    overlayColor: {
+      type: 'string',
+      format: 'color',
+      description: 'Overlay color to fade main site content'
+    },
+    closeIconSize: {
+      type: 'string',
+      description: 'Drawer close icon size'
+    },
+    closeIconColor: {
+      type: 'string',
+      format: 'color',
+      description: 'Drawer close icon color'
+    },
+    closeIconBackgroundColor: {
+      type: 'string',
+      format: 'color',
+      description: 'Drawer close icon background'
+    },
+    closeIconPosition: {
+      type: 'string',
+      enum: ['left', 'right'],
+      description: 'Position of close icon'
+    },
+    closeIconTop: {
+      type: 'string',
+      description: 'CSS top position of icon, 0 is default'
+    },
+    closeIconView: {
+      type: 'string',
+      enum: ['inside', 'outside'],
+      description: 'Close icon view'
+    },
+    headerText: {
+      type: 'string',
+      description: 'Set the text of Cart Header'
+    },
+    headerColor: {
+      type: 'string',
+      format: 'color',
+      description: 'Header text color'
+    },
+    headerFontSize: {
+      type: 'string',
+      description: 'Header text font-size'
+    },
+    headerFontFamily: {
+      type: 'string',
+      description: 'Set the Cart Header font-family'
+    },
+    headerTextAlign: {
+      type: 'string',
+      enum: ['left', 'center', 'right'],
+      description: 'Header text-align'
+    },
+    headerMargin: {
+      type: 'string',
+      description: 'Valid CSS margin for header'
+    },
+    headerTextTransform: {
+      type: 'string',
+      enum: ['uppercase', 'capitalize', 'lowercase', 'none'],
+      description: 'Set valid CSS text-transform'
+    },
+    itemText: {
+      type: 'string',
+      description: 'Set own Item text'
+    },
+    quantityText: {
+      type: 'string',
+      description: 'Set own Quantity text'
+    },
+    productFontSize: {
+      type: 'string',
+      description: 'Set product title font size'
+    },
+    productFontFamily: {
+      type: 'string',
+      description: 'Set product title font-family'
+    },
+    productColor: {
+      type: 'string',
+      format: 'color',
+      description: 'Set product title color'
+    },
+    variantFontSize: {
+      type: 'string',
+      description: 'Set product variant font size'
+    },
+    variantFontFamily: {
+      type: 'string',
+      description: 'Set product variant font-family'
+    },
+    variantColor: {
+      type: 'string',
+      format: 'color',
+      description: 'Set variant options colors'
+    },
+    pricesPreText: {
+      type: 'string',
+      description: 'Add some text to prices'
+    },
+    pricesFontFamily: {
+      type: 'string',
+      description: 'Price font-family'
+    },
+    pricesFontSize: {
+      type: 'string',
+      description: 'Price font size'
+    },
+    pricesColor: {
+      type: 'string',
+      format: 'color',
+      description: 'Price color'
+    },
+    subTotalAlign: {
+      type: 'string',
+      enum: ['left', 'center', 'right'],
+      description: 'Subtotal text-align'
+    },
+    subTotalFontSize: {
+      type: 'string',
+      description: 'Subtotal font-size'
+    },
+    subTotalFontFamily: {
+      type: 'string',
+      description: 'Subtotal font-family'
+    },
+    subTotalText: {
+      type: 'string',
+      description: 'Set the subtotal text'
+    },
+    subTotalColor: {
+      type: 'string',
+      format: 'color',
+      description: 'Set the subtotal text color'
+    },
+    checkoutButtonText: {
+      type: 'string',
+      description: 'Set the text of checkout Button'
+    },
+    checkoutButtonFontSize: {
+      type: 'string',
+      description: 'Checkout button font-size'
+    },
+    checkoutButtonFontFamily: {
+      type: 'string',
+      description: 'Checkout button font-family'
+    },
+    checkoutButtonColor: {
+      type: 'string',
+      format: 'color',
+      description: 'Checkout button color'
+    },
+    checkoutButtonBackgroundColor: {
+      type: 'string',
+      format: 'color',
+      description: 'Checkout button background color'
+    },
+    checkoutButtonAlign: {
+      type: 'string',
+      enum: ['left', 'center', 'right'],
+      description: 'Checkout button align'
+    },
+    showIfCartUpdated: {
+      type: 'boolean',
+      description: 'Show drawer if product was added/removed from cart'
+    },
+    restrictShowOnUpdateUrls: {
+      type: 'string',
+      description: 'URLs where you do not want Cart Drawer to appear after each cart update'
+    },
+    closeOnOverlayClick: {
+      type: 'boolean',
+      description: 'Close drawer if site or overlay is clicked'
+    },
+    onUpdateFunction: {
+      type: ['null', 'string'],
+      description: 'Function called on each cart update and on initial build, has drawer YUI element and cart JSON data in callback: function(el,data){}'
     }
   },
 };
